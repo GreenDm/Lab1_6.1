47dc1d7c64914919cfedd3182bddaee13554505f
HrinchenkoDmytro <d.greenchenko@gmail.com>
2016-02-22 11:52:01 +0200

    Initial commit

---
+++ src/epan/com/Main.java
@@ -0,0 +1,21 @@
+package epan.com;
+
+import java.util.Arrays;
+
+/**
+ * Created by Любовь on 22.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] arr1 = {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30};
+
+        Arrays.sort(arr1);
+
+        System.out.println("");
+        System.out.println(Arrays.toString(arr1));
+
+    }
+}
